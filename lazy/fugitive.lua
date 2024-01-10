92304187b2c05b7d0fb9b3a1e4a1eb1cc51db3fc
Khoi Le <khoile9826@gmail.com>
2024-01-09 23:38:45 -0500

    added more remaps

--- lazy/fugitive.lua
+++ lazy/fugitive.lua
@@ -1,3 +1,15 @@
 return {
-  { url="https://tpope.io/vim/fugitive.git" }
+  { 
+    url="https://tpope.io/vim/fugitive.git",
+    config = function()
+      vim.keymap.set('n', '<leader>P',
+        function()
+          vim.cmd.Git({'pull',  '--rebase'})
+        end, {})
+      vim.keymap.set('n', '<leader>gs',
+        function() 
+          vim.cmd.Git({'status'})
+        end, {})
+    end
+  }
 }
--- lazy/telescope.lua
+++ lazy/telescope.lua
@@ -7,6 +7,7 @@ return {
       require('telescope').setup {}
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+      vim.keymap.set('n', '<C-p>', builtin.git_files, {})
     end
   }
 }
