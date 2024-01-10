a2f5ddf917dc9fbc56437b895e41d25192d2e17b
Khoi Le <khoile9826@gmail.com>
2024-01-09 23:13:43 -0500

    init commit
    init commit

---
+++ init.lua
@@ -0,0 +1,5 @@
+require("khoinvim.remap")
+require("khoinvim.set")
+require("khoinvim.lazy_init")
+
+vim.cmd[[colorscheme tokyonight]]
---
+++ lazy/plugin.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  }
+}
